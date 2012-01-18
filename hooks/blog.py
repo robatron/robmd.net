4b3d313566275eba9dbaa58ee43c3b23b1f4bf69
Rob McGuire-Dale <rob.mcguiredale+git@gmail.com>
2012-01-17 21:53:35 -0800

    Finished process_title_images using site.content.gather.post hook.

--- hooks/blog.py
+++ hooks/blog.py
@@ -3,34 +3,32 @@ def process_title_images(pages):
     from Picasa Web Albums, if it exists, and create two new variables for use
     in the templates called `title_img_full` and `title_img_resized`
     '''
-    pass
 
-''' Old attempt using page.template.post hook
-
-    # declare standard image width, in pixels, for title images
     IMG_WIDTH = 480
 
-    try:
-        # grab the Picasa Web Albums' title image source URL from the 
-        # `title_img_src` variable in the page variables
-        src = templ_vars['page']['title_img_src']
-        src_split = src.split('/')
+    for page in pages:
+        try:
+            # grab the Picasa Web Albums' title image source URL from the 
+            # 'title_img_src' variable in the page's meta data
+            src = page.meta['title_img_src']
+            src_split = src.split('/')
 
-        # figure out URL portion before and after the 'size' property denoted 
-        # by /s<number>/ in the source URL which should look something like
-        # https://lh6.googleusercontent.com/foo/bar/herp/derp/s144/capture.png
-        src_before_size = '/'.join(src_split[:-2])
-        src_after_size = src_split[-1]
+            # figure out URL portion before and after the 'size' property 
+            # denoted by /s<number>/ in the source URL which should look 
+            # something like
+            # https://googleusercontent.com/foo/bar/herp/derp/s144/capture.png
+            src_before_size = '/'.join(src_split[:-2])
+            src_after_size = src_split[-1]
 
-        # construct the resized and full versions of the original source URL
-        src_resized = "%s/s%s/%s"%(src_before_size, IMG_WIDTH, src_after_size)
-        src_full = '%s/%s'%(src_before_size, src_after_size)
+            # construct the resized/full versions of the src URL
+            src_resized = "%s/s%s/%s"%(src_before_size, IMG_WIDTH,\
+                    src_after_size)
+            src_full = '%s/%s'%(src_before_size, src_after_size)
 
-        # insert the new variables for the template
-        templ_vars['page']['title_img_full'] = src_full
-        templ_vars['page']['title_img_resized'] = src_resized
+            # insert the new variables for the template
+            page.meta['title_img_full'] = src_full
+            page.meta['title_img_resized'] = src_resized
 
-    except KeyError:
-        # ignore any page that doesn't have a `title_img_src` variable
-        pass
-'''
+        except KeyError:
+            # ignore any page that doesn't have a `title_img_src` variable
+            pass
