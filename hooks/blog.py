3506c63010bd9dc5a8f2b07a804f0dc73d5bf663
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2012-02-03 14:15:37 -0800

    Adding thumbnail resize support for title image processing.

--- hooks/blog.py
+++ hooks/blog.py
@@ -6,8 +6,9 @@ def process_images(pages):
     ''' Process images on each blog article '''
 
     # default image sizes
-    TITLE_IMG_SIZE = 480
-    ARTICLE_IMG_SIZE = 320
+    TITLE_IMG_SIZE = 719
+    THUMBNAIL_IMG_SIZE = 100
+    ARTICLE_IMG_SIZE = 480
 
     def run():
         ''' Process images on all blog articles. Called at the start of 
@@ -15,24 +16,29 @@ def process_images(pages):
         '''
         for page in pages:
             if 'blog' in page.meta['category']:
-                process_title_image(page, TITLE_IMG_SIZE)
+                process_title_image(page, TITLE_IMG_SIZE, THUMBNAIL_IMG_SIZE)
                 process_article_images(page, ARTICLE_IMG_SIZE)
 
 
-    def process_title_image(page, size):
-        ''' For every blog article,  process the `title_img_src` in the YAML 
-        header from Picasa Web Albums, if it exists, and create two new 
-        variables for use in the templates called `title_img_full` and 
-        `title_img_resized`
+    def process_title_image(page, title_size, thumbnail_size):
+        ''' For every blog article, process the `title_img_src` in the YAML 
+        header from Picasa Web Albums, if it exists, and create three new 
+        variables for use in the templates:
+
+            `title_img_full`: Full-sized version of the image 
+            `title_img_title`: Title image-size version
+            `title_img_thumb`: Tiny version for use with thumbnails
         '''
 
         try:
             src = page.meta['title_img_src']
-            src_resized, src_full = split_picasaweb_src(src, size)
+            split_src_title = split_picasaweb_src(src, title_size) 
+            split_src_thumb = split_picasaweb_src(src, thumbnail_size)
 
             # insert the new variables for the template
-            page.meta['title_img_resized'] = src_resized
-            page.meta['title_img_full'] = src_full
+            page.meta['title_img_full'] = split_src_title['full']
+            page.meta['title_img_resized'] = split_src_title['resized']
+            page.meta['title_img_thumb'] = split_src_thumb['resized']
 
         except KeyError:
             # ignore any article that doesn't have a `title_img_src` variable
@@ -69,10 +75,11 @@ def process_images(pages):
         # defined in PICASAWEB_IMG_REPL
         matches = re.findall(PICASAWEB_IMG_FIND%('.*'), content, re.MULTILINE)
         for match in matches:
-            src_resized, src_full = split_picasaweb_src(match, img_size)
+            split_src = split_picasaweb_src(match, img_size)
 
             find = PICASAWEB_IMG_FIND%(match)
-            replace = PICASAWEB_IMG_REPL%(src_full, src_resized)
+            replace = PICASAWEB_IMG_REPL%(split_src['full'], 
+                    split_src['resized'])
 
             content = re.sub(find, replace, content, flags=re.MULTILINE) 
 
@@ -81,9 +88,8 @@ def process_images(pages):
 
 
     def split_picasaweb_src(src, size):
-        ''' Split a Picasa Web Album's image URL source into a resized and full
-        version of that URL. The resized version will be of size `size`. The
-        source URL should look something like
+        ''' Split a Picasa Web Album's image URL source into two sizes: full,
+        and the specified `size`. The source URL should look something like
         https://googleusercontent.com/foo/bar/herp/derp/s144/capture.png
         '''
 
@@ -94,13 +100,12 @@ def process_images(pages):
         src_before_size = '/'.join(src_split[:-2])
         src_after_size = src_split[-1]
 
-        # construct the resized/full versions of the src URL
-        src_resized = "%s/s%s/%s"%(src_before_size, size,
-                src_after_size)
+        # construct the full and resized versions of the src URL
         src_full= "%s/s%s/%s"%(src_before_size, 0, src_after_size)
+        src_resized = "%s/s%s/%s"%(src_before_size, size, src_after_size)
 
-        # return a tuple of the resized and full versions of the source URL
-        return (src_resized, src_full)
+        # return the full and resized versions of the source URL 
+        return {'full': src_full, 'resized': src_resized}
 
 
     # run all image processing functions
