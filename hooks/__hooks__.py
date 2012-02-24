9d683b11362e88238630d479cf6d1e01c9c3e3b5
Rob McGuire-Dale <rob.mcguiredale+git@gmail.com>
2012-02-24 01:29:03 -0800

    [style] Adding Sass complilation hook.

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -1,7 +1,9 @@
 ''' Attach functions to wok hooks '''
 
 import blog_images
+import sass
 
 hooks = {
+    'site.output.post':[sass.compile_css],
     'site.content.gather.post': [blog_images.process_picasaweb_images]
 }
---
+++ hooks/sass.py
@@ -0,0 +1,30 @@
+'''
+Plugin for compiling Sass files -> CSS
+
+Dependencies:
+
+    - Ruby
+    - Sass (http://sass-lang.com)
+'''
+import os
+import subprocess
+
+def compile_css(output_dir):
+    '''
+    Compile Sass files -> CSS in the output directory.
+
+    Any .scss or .sass files found in the output directory will be compiled 
+    to CSS using Sass. The compiled version of the file will be created in the 
+    same directory as the Sass file with the same name and an extension of 
+    .css. For example, foo.scss -> foo.css.
+    '''
+    print "--"
+    for root, dirs, files in os.walk(output_dir):
+        for f in files:
+            fname, fext = os.path.splitext(f)
+            if fext == ".scss" or fext == ".sass":
+                abspath = os.path.abspath(root)
+                sass_src = "%s/%s"%(abspath, f)
+                sass_dest = "%s/%s.css"%(abspath, fname)
+                sass_arg = "%s:%s"%(sass_src, sass_dest)
+                subprocess.call(['sass', sass_arg])
