dc5312bddc6ca96a762172fc5808f45ab5217c74
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2012-10-06 16:08:10 -0700

    Removing SASS hook for now

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -4,6 +4,5 @@ from wok.contrib.hooks import compile_sass
 import blog_images
 
 hooks = {
-    'site.output.post':[compile_sass],
     'site.content.gather.post': [blog_images.process_picasaweb_images]
 }
