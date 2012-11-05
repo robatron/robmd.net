1bafab931ce694598f755e70a64656f9cba7f155
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2012-11-04 16:43:24 -0800

    Move Less -> serverside

--- build.py
+++ build.py
@@ -7,16 +7,19 @@ Generate robmd.net with Wok from build directory
 import sys
 import os
 import subprocess
-import fnmatch
+import glob
 
 print "Building RobMD.net..."
+
+# Settings
+# --------
 
 # Location of the required Python modules and Node libraries
 BUILDPATH = 'build'
 PYPATH = os.path.join(BUILDPATH, 'python')
 NODEPATH = os.path.join(BUILDPATH, 'node')
 
-# Python modules and Node packages
+# Python modules
 PYMODS = [
     'wok-master',
     os.path.join('PyYAML-3.10', 'lib'),
@@ -24,23 +27,27 @@ PYMODS = [
     'Jinja2-2.6',
 ]
 
+# Python init
+# -----------
 print "Adding Python modules to the Python path..."
 for module in PYMODS:
     mod_dir = os.path.join(os.getcwd(), PYPATH, module)
     sys.path.append(mod_dir)
     print "\tAdded %s"%mod_dir
 
-# Compile Less stylesheets
-LESSC = os.path.join(NODEPATH, 'less-1.3.1', 'bin', 'lessc')
-LESSDIR = os.path.join('media', 'less')
-LESSFILES = os.listdir(LESSDIR)
-
+# Less
+# ----
 print "Compiling Less stylesheets..."
-for lessfile in LESSFILES:
-    if fnmatch.fnmatch(lessfile, '*.less'):
-        subprocess.call(['node', LESSC, '--compress', lessfile, lessfile + '.css'])
-        print "Compiled %s"%lessfile
 
+LESSC = os.path.join(NODEPATH, 'less-1.3.1', 'bin', 'lessc')
+LESSFILES = glob.glob(os.path.join('media', 'less', '*.less'))
+
+for lessfile in LESSFILES:
+    subprocess.call(['node', LESSC, '--compress', lessfile, lessfile + '.css'])
+    print "\tCompiled %s"%lessfile
+
+# Wok
+# ---
 print "Running Wok..."
 from wok.engine import Engine
 Engine()
